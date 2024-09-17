6da580160fe89d4d8bba6fa0f115cf8f0312340a
ErdeiGergo <21.erdei.gergo.janos@mechwart.com>
2024-09-17 13:53:14 +0200

    Pages have been deleted

--- Projekt/Projekt/Admin.xaml.cs
+++ Projekt/Projekt/Admin.xaml.cs
@@ -23,5 +23,10 @@ namespace Projekt
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            myFrame.Content = new Tantargy();
+        }
     }
 }
