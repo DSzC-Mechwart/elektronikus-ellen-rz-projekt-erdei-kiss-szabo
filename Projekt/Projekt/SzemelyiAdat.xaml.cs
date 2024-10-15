c5a0e2cfca1f178009ee9bdc7c8b5e1df3e80d5a
szabo <szabo.balazs1214@gmail.com>
2024-10-15 13:44:27 +0200

    Working input

--- Projekt/Projekt/SzemelyiAdat.xaml.cs
+++ Projekt/Projekt/SzemelyiAdat.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Projekt
 {
@@ -19,11 +21,14 @@ namespace Projekt
     /// </summary>
     public partial class SzemelyiAdat : Window
     {
+        bool Kollegista = false;
+        List<szAdat> KorabbiAdatok;
         public SzemelyiAdat()
         {
             InitializeComponent();
             szuletesiIdo.SelectedDate = DateTime.Now;
             beiratkozas.SelectedDate = DateTime.Now;
+            
         }
         private void visszaGomb_Click(object sender, RoutedEventArgs e)
         {
@@ -33,15 +38,64 @@ namespace Projekt
 
         private void kollegista_Changed(object sender, RoutedEventArgs e)
         {
-            kollegium.Visibility = (kollegium.Visibility==Visibility.Hidden) ? Visibility.Visible : Visibility.Hidden;
-            kollegiumLabel.Visibility = (kollegiumLabel.Visibility==Visibility.Hidden) ? Visibility.Visible : Visibility.Hidden;
+            if (kollegium.Visibility==Visibility.Hidden)
+            {
+                kollegium.Visibility=Visibility.Visible;
+                Kollegista = true;
+            }
+            else
+            {
+                kollegium.Visibility = Visibility.Hidden;
+                Kollegista = false;
+            }
+        }
+
+        private void KorabbiData()
+        {
+            KorabbiAdatok = [];
+            foreach (var item in File.ReadAllLines("SzemelyiAdatok.csv", Encoding.UTF8).Skip(1))
+            {
+                string[] sor = item.Split(';');
+                string Nev = sor[0];
+                string SzuletesiHely = sor[1];
+                DateOnly SzuletesiIdo = DateOnly.FromDateTime(Convert.ToDateTime(sor[2]));
+                string AnyjaNeve = sor[3];
+                string Lakcim = sor[4];
+                DateOnly Beiratkozas = DateOnly.FromDateTime(Convert.ToDateTime(sor[5]));
+                string Szak = sor[6];
+                string Osztaly = sor[7];
+                bool Kollegista = (sor[8] == "Igen") ? true : false;
+                string Kollegium = sor[9];
+                string TorzslapSzam = sor[10];
+                szAdat Adat = new(Nev, SzuletesiHely, SzuletesiIdo, AnyjaNeve, Lakcim, Beiratkozas, Szak, Osztaly, Kollegista, Kollegium, TorzslapSzam);
+                KorabbiAdatok.Add(Adat);
+            }
         }
 
         private void leadasGomb_Click(object sender, RoutedEventArgs e)
         {
+            KorabbiData();
             string Nev = nev.Text;
             string SzuletesiHely = szuletesiHely.Text;
             DateOnly SzuletesiIdo = DateOnly.FromDateTime(szuletesiIdo.SelectedDate!.Value);
+            string AnyjaNeve = anyjaNeve.Text;
+            string Lakcim = lakcim.Text;
+            DateOnly Beiratkozas = DateOnly.FromDateTime(beiratkozas.SelectedDate!.Value);
+            string Szak = szak.Text;
+            string Osztaly = osztaly.Text;
+            string Kollegium = (Kollegista) ? kollegium.Text : "Nincs";
+            int SorSzam = Convert.ToInt32(File.ReadLines("SzemelyiAdatok.csv").First());
+            string TorzslapSzam = $"{SorSzam}/{Beiratkozas.Year}";
+            szAdat Adat = new(Nev, SzuletesiHely, SzuletesiIdo, AnyjaNeve, Lakcim, Beiratkozas, Szak, Osztaly, Kollegista, Kollegium, TorzslapSzam);
+            using (StreamWriter writer = new StreamWriter("SzemelyiAdatok.csv"))
+            {
+                writer.WriteLine($"{SorSzam+1}");
+                foreach(var item in KorabbiAdatok)
+                {
+                    writer.WriteLine($"{item.Nev};{item.SzuletesiHely};{item.SzuletesiIdo};{item.AnyjaNeve};{item.Lakcim};{item.BeiratkozasIdeje};{item.Szak};{item.Osztaly};{((item.Kollegista) ? "Igen" : "Nem")};{item.Kollegium};{item.Torzslapszam}");
+                }
+                writer.WriteLine($"{Adat.Nev};{Adat.SzuletesiHely};{Adat.SzuletesiIdo};{Adat.AnyjaNeve};{Adat.Lakcim};{Adat.BeiratkozasIdeje};{Adat.Szak};{Adat.Osztaly};{((Adat.Kollegista) ? "Igen" : "Nem")};{Adat.Kollegium};{Adat.Torzslapszam}"); 
+            }
         }
     }
 }
--- Projekt/Projekt/szAdat.cs
+++ Projekt/Projekt/szAdat.cs
@@ -8,7 +8,7 @@ namespace Projekt
 {
     class szAdat
     {
-        public szAdat(string nev, string szuletesiHely, DateTime szuletesiIdo, string anyjaNeve, string lakcim, DateTime beiratkozasIdeje, string szak, string osztaly, bool kollegistaCheck, string kollegium, string torzslapszam)
+        public szAdat(string nev, string szuletesiHely, DateOnly szuletesiIdo, string anyjaNeve, string lakcim, DateOnly beiratkozasIdeje, string szak, string osztaly, bool kollegista, string kollegium, string torzslapszam)
         {
             Nev = nev;
             SzuletesiHely = szuletesiHely;
@@ -18,20 +18,20 @@ namespace Projekt
             BeiratkozasIdeje = beiratkozasIdeje;
             Szak = szak;
             Osztaly = osztaly;
-            KollegistaCheck = kollegistaCheck;
+            Kollegista = kollegista;
             Kollegium = kollegium;
             Torzslapszam = torzslapszam;
         }
 
         public string Nev { get; set; }
         public string SzuletesiHely { get; set; }
-        public DateTime SzuletesiIdo { get; set; }
+        public DateOnly SzuletesiIdo { get; set; }
         public string AnyjaNeve { get; set; }
         public string Lakcim {  get; set; }
-        public DateTime BeiratkozasIdeje {  get; set; }
+        public DateOnly BeiratkozasIdeje {  get; set; }
         public string Szak {  get; set; }
         public string Osztaly {  get; set; }
-        public bool KollegistaCheck {  get; set; }
+        public bool Kollegista {  get; set; }
         public string Kollegium {  get; set; }
         public string Torzslapszam {  get; set; }
     }
