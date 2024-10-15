ebe7acde6345a2bc213b2278c54cb98a70d7ccef
KBencee <kissbence225@gmail.com>
2024-10-15 09:15:47 +0200

    aaa

--- Projekt/Projekt/AdminPage.xaml.cs
+++ Projekt/Projekt/AdminPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,7 +25,7 @@ namespace Projekt
         {
             InitializeComponent();
 
-            DataContext = Tantargyak.ReadFile("tantargyak.csv");
+            DataContext = Tantargyak.Read("tantargyak.csv");
         }
     }
 }
