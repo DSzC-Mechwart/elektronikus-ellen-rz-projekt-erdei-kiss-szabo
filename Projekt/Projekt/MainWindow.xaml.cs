53a351d1c35ae01612916a1b9b19337843ba99d1
KBencee <kissbence225@gmail.com>
2024-10-21 19:59:14 +0200

    bejelentkezés felig kész

--- Projekt/Projekt/MainWindow.xaml.cs
+++ Projekt/Projekt/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,34 +21,55 @@ namespace Projekt
         {
             InitializeComponent();
             diakbej.IsEnabled = false;
-            //adminbej.IsEnabled = false;
+            adminbej.IsEnabled = false;
         }
 
         private void diakbe(object sender, RoutedEventArgs e)
         {
-            new Diak().Show();
-            Close();
+            string enteredName = neve.Text;
+            string enteredPassword = jelszava.Text;
+
+            bool loginSuccess = false;
+            foreach (var item in File.ReadAllLines("SzemelyiAdatok.csv", Encoding.UTF8).Skip(1))
+            {
+                string[] sor = item.Split(';');
+                string nev = sor[0];
+                string hely = sor[1];
+
+                if (enteredName == nev && enteredPassword == hely)
+                {
+                    loginSuccess = true;
+                    break;
+                }
+            }
+
+            if (loginSuccess)
+            {
+                new Diak().Show();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Hibás név vagy jelszó!");
+            }
         }
 
         private void adminbe(object sender, RoutedEventArgs e)
         {
             new Admin().Show();
+            adminbej.IsEnabled = false;
             Close();
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            diakbej.IsEnabled = true;
+            diakbej.IsEnabled = !string.IsNullOrWhiteSpace(neve.Text) && !string.IsNullOrWhiteSpace(jelszava.Text);
             string adminnev = neve.Text;
-            string diaknev = neve.Text;
             string adminjelszo = jelszava.Text;
-            string diakjelszo = jelszava.Text;
             if (adminnev == "admin" && adminjelszo == "admin") 
             {
                 adminbej.IsEnabled = true;
             }
-            if (diaknev == "" || diakjelszo == "")
-                diakbej.IsEnabled = false;
         }
 
         private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
