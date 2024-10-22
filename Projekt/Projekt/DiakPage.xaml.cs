54fb96b197d9ba217661b181802a8d4194e3d3ce
KBencee <kissbence225@gmail.com>
2024-10-22 07:06:38 +0200

    a heten befejezem

--- Projekt/Projekt/DiakPage.xaml.cs
+++ Projekt/Projekt/DiakPage.xaml.cs
@@ -15,18 +15,42 @@ using System.Windows.Shapes;
 
 namespace Projekt
 {
-    /// <summary>
-    /// Interaction logic for DiakPage.xaml
-    /// </summary>
+    public class Jegy
+    {
+        public string Tantargy { get; set; }
+        public int ÉrdemJegy { get; set; }
+        public string Tema { get; set; }
+        public string SzamonkeresTipusa { get; set; }
+    }
+
     public partial class DiakPage : Page
     {
+        private string bejelentkezettDiak = "Kis Pista";
+
+        private Dictionary<string, Dictionary<string, List<int>>> diakokJegyei;
+
         public DiakPage()
         {
             InitializeComponent();
-        }
 
+            diakokJegyei = new Dictionary<string, Dictionary<string, List<int>>>
+            {
+                { "Geri", new Dictionary<string, List<int>> { { "Történelem", new List<int> { 4, 2 } }, { "Matematika", new List<int> { 3, 5 } } } },
+                { "Anna", new Dictionary<string, List<int>> { { "Ének", new List<int> { 1, 1 } }, { "Tánc", new List<int> { 2, 4 } } } },
+                { "Petra", new Dictionary<string, List<int>> { { "Szoftver tesztelés", new List<int> { 3, 2 } }, { "Programozás", new List<int> { 4, 4 } } } },
+                { "Kis Pista", new Dictionary<string, List<int>> { { "Fizika", new List<int> { 5, 4 } }, { "Kémia", new List<int> { 3, 2 } } } }
+            };
+
+        }
         private void JegyHozzaadasa(object sender, RoutedEventArgs e)
         {
+            string selectedTargy = targyComboBox.SelectedItem?.ToString();
+            int selectedJegy = int.Parse((jegyComboBox.SelectedItem as ComboBoxItem)?.Content.ToString());
+
+            if (diakokJegyei.ContainsKey(bejelentkezettDiak) && diakokJegyei[bejelentkezettDiak].ContainsKey(selectedTargy))
+            {
+                diakokJegyei[bejelentkezettDiak][selectedTargy][0] += selectedJegy;
+            }
 
         }
     }
